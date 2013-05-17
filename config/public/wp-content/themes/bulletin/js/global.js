0d7dc84ed45ee0c81154c65900b50673330abf3c
Ethan Kaplan <ethan@livenationlabs.com>
2013-05-17 13:54:40 -0700

    new theme

---
+++ config/public/wp-content/themes/bulletin/js/global.js
@@ -0,0 +1,10 @@
+jQuery(function($){
+	$(document).ready(function(){
+		function wpexFancyBoxInit() {
+			$(".fancybox").fancybox({
+				openEffect	: 'none',
+				closeEffect	: 'none'
+			});
+		} wpexFancyBoxInit();
+	});
+});
